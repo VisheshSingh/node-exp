8e34aa4101ab201c7c53b8c3a5c78d9c3ca38869
Vishesh <VisheshSingh16@gmail.com>
2020-01-12 20:47:01 -0600

    HTTP delete request

--- index.js
+++ index.js
@@ -53,6 +53,17 @@ app.put('/api/courses/:id', (req, res) => {
   res.send(course);
 });
 
+app.delete('/api/courses/:id', (req, res) => {
+  const course = courses.find(course => course.id === parseInt(req.params.id));
+
+  if (!course)
+    res.status(404).send('Course with requested id cannot be deleted! 😣');
+
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+  res.send(course);
+});
+
 function validateCourse(course) {
   const schema = {
     name: Joi.string()
